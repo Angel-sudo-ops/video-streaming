3f8b8b10a576993862b9fb93b7721ce664cc2c71
Angel-sudo-ops <59897011+Angel-sudo-ops@users.noreply.github.com>
2020-06-30 15:58:35 -0500

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -11,6 +11,7 @@ $(function() {
 		$.getJSON('/commands/' + $(this).val(), function(data) {
 			//do  something
 		});
+		/*Timeout function for changing list value to Select... in 1s each time it changes its value*/
 		setTimeout(function() {$('#preset').val('Select...');},1000);
 		return false;
 	});
